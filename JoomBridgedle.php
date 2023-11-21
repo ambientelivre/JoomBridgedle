2bbe2724ba1a0a45714aa7693a3e8d1436fe249d
Miguel Domiciano Vieira <97977665+Migueldv06@users.noreply.github.com>
2023-11-21 16:17:05 -0300

    Update and rename APImoodle.php to JoomBridgedle.php

--- JoomBridgedle.php
+++ JoomBridgedle.php
@@ -2,16 +2,25 @@
 
 defined( '_JEXEC' ) or die;
 
-class PlgContentAPImoodle extends JPlugin
+class PlgContentJoomBridgedle extends JPlugin
 {
 
     public function onContentPrepare($context, &$article, &$params, $page = 0) {
-        $regex = '/\[meushortcode1\]/i';
+
+        //recebe o shortcode
+        $short1 = ($this->params->get('shortcode1'));
+
+        //verifica se o shortcode esta no texto do artigo , se sim inicia o codigo 1
+        $regex = "{{$short1}}";
         if (preg_match($regex, $article->text)) {
             $article->text = preg_replace($regex, $this->processShortcode1(), $article->text);
         }
-    
-        $regex = '/\[meushortcode2\]/i';
+        
+        //recebe o shortcode
+        $short2= ($this->params->get('shortcode2'));
+
+        //verifica se o shortcode esta no texto do artigo , se sim inicia o codigo 2
+        $regex = "{{$short2}}";
         if (preg_match($regex, $article->text)) {
             $article->text = preg_replace($regex, $this->processShortcode2(), $article->text);
         }
@@ -19,33 +28,41 @@ class PlgContentAPImoodle extends JPlugin
     
     private function processShortcode1() {
         // Código para o primeiro shortcode
+
+        //recebe dados da url e coloca na variavel $resultado
         $URL1 = ($this->params->get('URL1'));
         $ch = curl_init($URL1);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         $resultado = json_decode(curl_exec($ch));
 
+        //conta quantas vezes apareceu a variavel $alunos
         $numaluno = 0 ;
         foreach ($resultado as $contaarray){
             $numaluno++;
         }
-        $conteudo = $numaluno;
-        return $conteudo;
+
+        //retorna a quantidade de alunos no curso
+        return $numaluno;
     }
     
     private function processShortcode2() {
         // Código para o segundo shortcode
+
+        //recebe dados da url e coloca na variavel $resultado
         $URL2 = ($this->params->get('URL2'));
         $ch = curl_init($URL2);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         $resultado = json_decode(curl_exec($ch));
 
+        //conta quantas vezes apareceu a variavel $alunos
         $numaluno = 0 ;
-        foreach ($resultado as $contaarry){
+        foreach ($resultado as $contaarray){
             $numaluno++;
         }
 
+        //retorna a quantidade no curso
         return $numaluno;
     }
     
